4507eb90659040ca9706b72d2771f7dc6dcd207d
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-06-02 21:52:17 -0400

    Updated the test URL

--- spec/test.sh
+++ spec/test.sh
@@ -7,7 +7,7 @@ __self="${BASH_SOURCE[0]}"
 __dir="$(cd "$(dirname "${__self}")" > /dev/null && pwd)"
 __file="${__dir}/$(basename "${__self}")"
 
-dlurl='https://anonfiles.com/41B4X1Zdye/jmmb_avatar_png'
+dlurl='https://anonfiles.com/q9W8X9u3za/jmmb_avatar_png'
 
 anonfilesdl="$(dirname "${__dir}")/anonfilesdl"
 tmpdir="${__dir}/tmp"
