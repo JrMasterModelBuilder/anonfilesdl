00f11bd71dc248b12bab2fec7bf1464a7206f250
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-07-14 23:36:42 -0400

    Upload tiny test file each time

--- spec/test.sh
+++ spec/test.sh
@@ -7,17 +7,19 @@ __self="${BASH_SOURCE[0]}"
 __dir="$(cd "$(dirname "${__self}")" > /dev/null && pwd)"
 __file="${__dir}/$(basename "${__self}")"
 
-dlurl='https://anonfiles.com/q9W8X9u3za/jmmb_avatar_png'
-
 anonfilesdl="$(dirname "${__dir}")/anonfilesdl"
-tmpdir="${__dir}/tmp"
+cd "${__dir}"
+tmpdir='tmp'
+
+uploaded="$(curl -s -F 'file=@hello.txt' 'https://api.anonfiles.com/upload')"
+dlurl="$(jq -r '.data.file.url.full' <<< "${uploaded}")"
 
 rm -rf "${tmpdir}"
 mkdir "${tmpdir}"
 pushd "${tmpdir}" > /dev/null
 
 "${anonfilesdl}" "${dlurl}"
-shasum -c "${__dir}/jmmb_avatar.png.sha256"
+shasum -c "${__dir}/hello.txt.sha256"
 
 popd > /dev/null
 rm -rf "${tmpdir}"
